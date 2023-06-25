842be300da6f310615772f3451f125e5f5f01587
shafin bisswas <mdnazmussakibshafin@gmail.com>
2023-06-25 10:23:45 +0600

    first commit

---
+++ src/Pages/Home/Category/Category.jsx
@@ -0,0 +1,51 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { FreeMode, Pagination } from "swiper";
+
+import slide1 from '../../../assets/home/slide01.png'
+import slide2 from '../../../assets/home/slide02.png'
+import slide3 from '../../../assets/home/slide03.png'
+import slide4 from '../../../assets/home/slide04.png'
+import slide5 from '../../../assets/home/slide05.png'
+
+const Category = () => {
+    return (
+        // figma follow kore uporer lekha dibo
+        <Swiper
+            slidesPerView={4}
+            spaceBetween={30}
+            freeMode={true}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[FreeMode, Pagination]}
+            className="mySwiper my-10"
+        >
+            <SwiperSlide>
+                <img src={slide1} alt="" />
+                <h3 className="text-center text-4xl uppercase text-white -mt-16">Grill Stick</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide2} alt="" />
+                <h3 className="text-center text-4xl uppercase text-white -mt-16">Hot Dogs</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide3} alt="" />
+                <h3 className="text-center text-4xl uppercase text-white -mt-16">Grill Veg</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide4} alt="" />
+                <h3 className="text-center text-4xl uppercase text-white -mt-16">Wings</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide5} alt="" />
+                <h3 className="text-center text-4xl uppercase text-white -mt-16">Steaks</h3>
+            </SwiperSlide>
+        </Swiper>
+    );
+};
+
+export default Category;
---
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+
+    const navOptions = <>
+        <li><Link className="text-[17px] font-bold" to="/">Home</Link></li>
+        <li><Link className="text-[17px] font-bold" to="/contact">Contact Us</Link></li>
+        <li><Link className="text-[17px] font-bold" to="/dashboard">Dashboard</Link></li>
+        <li><Link className="text-[17px] font-bold" to="/menu">Our Menu</Link></li>
+        <li><Link className="text-[17px] font-bold" to="/shop">Our Shop</Link></li>
+    </>
+
+    return (
+        <div>
+            <div className="navbar fixed z-10 bg-opacity-30 max-w-screen-xl bg-black text-white">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {navOptions}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost normal-case text-[25px]">Happy Grill</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navOptions}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <button className="bg-blue-900 font-bold text-white px-[25px] rounded  py-[14px]">Login</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
